6efbd67b969181d554d7d0959f2c1a1c2d55676a
Victor Godinho <dragonsgamers@hotmail.com.br>
2023-02-11 21:16:49 -0300

    fix: :adhesive_bandage: fix publish script

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
-docker build ./lts --tag dragonsgamers/playwright-alpine:latest /
-    && docker run dragonsgamers/playwright-alpine:latest /
+docker build ./lts --tag dragonsgamers/playwright-alpine:latest \
+    && docker run dragonsgamers/playwright-alpine:latest \
     && docker push dragonsgamers/playwright-alpine:latest
 
 docker build ./16 --tag dragonsgamers/playwright-alpine:16 \
