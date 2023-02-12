59ddea834f5dbb333b05d0ea040cbdf9c838c9c3
Victor Godinho <dragonsgamers@hotmail.com.br>
2023-02-11 21:18:21 -0300

    fix: :bug: fix versions publish name

--- publish.sh
+++ publish.sh
@@ -5,3 +5,7 @@ docker build ./lts --tag dragonsgamers/playwright-alpine:latest \
 docker build ./16 --tag dragonsgamers/playwright-alpine:16 \
     && docker run dragonsgamers/playwright-alpine:16 \
     && docker push dragonsgamers/playwright-alpine:16
+
+docker build ./lts --tag dragonsgamers/playwright-alpine:18 \
+    && docker run dragonsgamers/playwright-alpine:18 \
+    && docker push dragonsgamers/playwright-alpine:18
