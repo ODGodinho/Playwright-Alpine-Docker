e3255810385a2845a5c45849b811fb0fd9cbaff3
Victor Godinho <victor.godinho@plataforma13.com.br>
2024-05-06 18:20:00 -0300

    feat: :sparkles: upgrade node 20 docker

--- publish.sh
+++ publish.sh
@@ -6,6 +6,10 @@ docker build ./16 --tag dragonsgamers/playwright-alpine:16 \
     && docker run dragonsgamers/playwright-alpine:16 \
     && docker push dragonsgamers/playwright-alpine:16
 
-docker build ./lts --tag dragonsgamers/playwright-alpine:18 \
+docker build ./18 --tag dragonsgamers/playwright-alpine:18 \
     && docker run dragonsgamers/playwright-alpine:18 \
     && docker push dragonsgamers/playwright-alpine:18
+
+docker build ./lts --tag dragonsgamers/playwright-alpine:20 \
+    && docker run dragonsgamers/playwright-alpine:20 \
+    && docker push dragonsgamers/playwright-alpine:20
